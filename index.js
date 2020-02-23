7cb3c3063b8c7b4bf9a3c1bfea67e706befa788c
Miguel Quiambao <miguel_quiambao@dlsu.edu.ph>
2020-02-23 10:03:14 +0800

    lol

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ app.engine('hbs', hbs({
 }))
 
 app.get('/', (req, res) => {
+  console.log(req.cookies)
   if (req.cookies["user"]) {
-    console.log(req.cookies["user"])
     firestore
       .collection("users")
       .where("email", "==", req.cookies["user"].email)
@@ -37,7 +37,7 @@ app.get('/', (req, res) => {
             res.cookie("user", doc.data())
           })
         }
-
+        
         if (req.cookies["user"] && req.cookies["user"].subscription && req.cookies["user"].subscription.start && req.cookies["user"].subscription.end) {
           res.render('home', {user: req.cookies['user'], template: 'home', layout: 'home'})
         } else if (req.cookies["user"]) {
@@ -45,6 +45,13 @@ app.get('/', (req, res) => {
         } else 
           res.render("index", {user: req.cookies['user'], template: 'landing'})
       })
+  } else {
+    if (req.cookies["user"] && req.cookies["user"].subscription && req.cookies["user"].subscription.start && req.cookies["user"].subscription.end) {
+      res.render('home', {user: req.cookies['user'], template: 'home', layout: 'home'})
+    } else if (req.cookies["user"]) {
+      res.render("complete_information", {user: req.cookies['user'], layout: 'complete_information', template: 'complete'})
+    } else 
+      res.render("index", {user: req.cookies['user'], template: 'landing'})
   }
 })
 
